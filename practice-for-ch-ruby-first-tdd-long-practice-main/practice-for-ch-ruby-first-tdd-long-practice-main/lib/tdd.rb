a4167daca53c5c515ebf29060c0dfb01ebf7a85f
mchung099 <mchung099@gmail.com>
2023-02-23 16:33:33 -0800

    stock picker

--- practice-for-ch-ruby-first-tdd-long-practice-main/practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -4,8 +4,6 @@ def uniq(array)
   counter.keys
 end
 
-p uniq([1, 2, 1, 3, 3] )
-
 def two_sum(array)
 
   pairs = []
@@ -14,8 +12,32 @@ def two_sum(array)
     ((i + 1)...array.length).to_a.each do |j|
       pairs << [i, j] if array[i] + array[j] == 0
     end
-  end    
+  end
   pairs
 end
 
+def my_transpose(array)
+  array.transpose
+end
 
+def stock_picker(prices)
+  pairs = []
+  days = Hash.new { |h, k| h[k] = [] }
+
+  prices.each_with_index do |price1, buy|
+    prices.each_with_index do |price2, sell|
+      if sell > buy
+        profit = price2 - price1
+        days[profit] << [buy, sell]
+      end
+    end
+  end
+  new_arr = days.max_by { |profit, day| day }
+  return [] if new_arr[1].count > 2
+  new_arr[1]
+  # !days.all? { |profit, day| day.length == 1 }
+end
+
+p stock_picker([1,1,1,1])
+p stock_picker([10,9,8,7])
+# how to change the code if we were to account for decreasing stock prices
--- practice-for-ch-ruby-first-tdd-long-practice-main/practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -19,11 +19,11 @@ describe Array do
     end
   end
 
-  describe "two_sum" do
+  describe "#two_sum" do
     subject (:array) {[-1, 0, 2, -2, 1]}
-      
-    context "it returns of positions of pairs of elements that sum to zero" do 
-      it "finds multiple pairs" do 
+
+    context "it returns of positions of pairs of elements that sum to zero" do
+      it "finds multiple pairs" do
         expect(two_sum(array)).to eq([[0, 4], [2, 3]])
       end
 
@@ -33,7 +33,7 @@ describe Array do
 
       it "returns empty array when there is no pair" do
         expect(two_sum([3,4])).to eq ([])
-      end  
+      end
     end
 
     context "it sorts the pairs with the smaller index before the bigger index" do
@@ -42,5 +42,36 @@ describe Array do
       end
     end
   end
-end
 
+  describe '#my_transpose' do
+    subject(:array) {[
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]}
+
+    it "returns a transposed array" do
+      expect(my_transpose(array)).to eq(array.transpose)
+    end
+  end
+
+  describe '#stock_picker' do
+    subject(:array) { [1, 2, 3, 4] }
+
+    context "should return a pair of most profitable days" do
+      it "returns an array in a pair" do
+        expect(stock_picker(array)).to eq([[0,3]])
+      end
+
+      it "the difference of the prices at the index of the pair returns most profit" do
+        expect(stock_picker([4,5,13,1])).to eq([[2,3]])
+      end
+    end
+
+    context "when there is no profit" do
+      it "should return an empty array" do
+        expect(stock_picker([1,1,1,1])).to eq([])
+      end
+    end
+  end
+end
